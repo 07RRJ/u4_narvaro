36fde2e9ea709365a70718429a12a533735af8b9
07RRJ <rasmusrudko07@gmail.com>
2026-05-25 21:29:47 +0200

    made it launch

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import helmet from 'helmet';
 import { fileURLToPath } from 'url';
 import { join, dirname } from 'path';
-import requestRoutes from './routes/request.routes.js';
+// import requestRoutes from './routes/request.routes.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -15,7 +15,9 @@ app.use(express.static(join(__dirname, 'public')));
 
 const PORT = Number(process.env.PORT || 3000);
 
-app.use('/', requestRoutes);
+// app.use('/', requestRoutes);
+app.use('/');
+
 
 app.listen(PORT, () => {
   console.log(`[server] http://localhost:${PORT}`);
