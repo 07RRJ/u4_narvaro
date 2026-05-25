197bfc2df32fc7be39c7c5fc255317d50e56a5d6
07RRJ <rasmusrudko07@gmail.com>
2026-05-25 20:15:15 +0200

    removed sendMessage

--- src/controllers/request.controller.js
+++ src/controllers/request.controller.js
@@ -6,37 +6,6 @@ import { successView } from '../views/success.view.js';
 import { messagesView } from '../views/messages.view.js';
 import { editView } from '../views/edit.view.js';
 
-
-
-export async function sendMessage(req, res) {
-  const name = clampString(req.body?.name, 50);
-  const message = clampString(req.body?.message, 500);
-
-  if (!name || !message) {
-    return res
-      .status(400)
-      .type('html')
-      .send('<p>Fel: saknar namn eller meddelande.</p><p><a href="/">Tillbaka</a></p>');
-  }
-
-  const userAgent = clampString(req.get('user-agent'), 200);
-  const ip = clampString(req.ip, 60);
-
-  const { error } = await supabase
-    .from('request_messages')
-    .insert([{ name, message, user_agent: userAgent, ip }]);
-
-  if (error) {
-    console.error('[supabase] insert error:', error);
-    return res
-      .status(500)
-      .type('html')
-      .send('<p>Serverfel när vi skulle spara i databasen.</p><p><a href="/">Tillbaka</a></p>');
-  }
-
-  res.type('html').send(successView(name, message));
-}
-
 export async function listMessages(req, res) {
   const { data, error } = await supabase
     .from('request_messages')
