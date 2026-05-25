cf25f1a9ec06a2611c653da940be6ecb20c48a0c
Cyber-Erkan <erik.ekryd@gmail.com>
2026-05-25 19:53:05 +0200

    deleted old structure

--- src/utils/html.js
+++
@@ -1,9 +0,0 @@
-// src/utils/html.js
-export function escapeHtml(s) {
-  return String(s)
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#039;');
-}
--- src/utils/sanitize.js
+++
@@ -1,5 +0,0 @@
-// src/utils/sanitize.js
-export function clampString(value, maxLen) {
-  const v = String(value ?? '').trim();
-  return v.length > maxLen ? v.slice(0, maxLen) : v;
-}
